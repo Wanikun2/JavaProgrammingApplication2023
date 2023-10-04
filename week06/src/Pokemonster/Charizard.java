c6d848c511e2916f83debf54f7d4c85608bfb634
Wanikun2 <aldalddl564@gmail.com>
2023-10-04 15:42:39 +0900

    week06 v29

--- week06/src/Pokemonster/Charizard.java
+++ week06/src/Pokemonster/Charizard.java
@@ -1,5 +1,7 @@
 package Pokemonster;
 
+import java.util.ArrayList;
+
 public class Charizard extends Pokemon{
     public Charizard(Flyable f) {
         System.out.println("자몽자몽~");
@@ -11,8 +13,21 @@ public class Charizard extends Pokemon{
         this.setHp(78);
         this.name = "리자몽";
         this.flyable = f; // Association, has-a (Aggregation)
-        this.skills = new String[]{"화염방사", "용의숨결", "할퀴기"};
-        this.specialAttackRate = new int[]{90, 60, 40};
+//        this.skills = new String[]{"화염방사", "용의숨결", "할퀴기"};
+//        this.specialAttackRate = new int[]{90, 60, 40};
+
+        // 1. Pokemon에 ArrayList 선언(만들기) 2. 각각 포켓몬에 skill, specialAttackRate(Pokemon에 만든) 거에 ArrayList 만들고 안에 들어갈 데이터 add로 넣기
+        // 3. Attack, PokemonGame 에 배열들 다 .get()로 꺼내기
+
+        this.skills = new ArrayList<>();
+        this.skills.add("화염방사");
+        this.skills.add("용의숨결");
+        this.skills.add("할퀴기");
+
+        this.specialAttackRate = new ArrayList<>();
+        this.specialAttackRate.add(90);
+        this.specialAttackRate.add(60);
+        this.specialAttackRate.add(40);
     }
 
     public Charizard(int level, int hp, String name) {
