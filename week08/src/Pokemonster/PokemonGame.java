e45ce8e2ecbfc4c94700e737d09c072774fd7e68
Wanikun2 <aldalddl564@gmail.com>
2023-10-18 14:31:35 +0900

    week08 v42

--- week08/src/Pokemonster/PokemonGame.java
+++ week08/src/Pokemonster/PokemonGame.java
@@ -81,12 +81,13 @@ public class PokemonGame {
     }catch (NullPointerException err) {
         System.out.println("플레이어 객체가 생성이 안되었습니다.");
         System.out.println("예외 내용 " + err.getMessage());
-
-//    } catch (Exception err){
-//        System.out.println("예외가 발생했습니다.");
-//        System.out.println("예외 내용 : " + err.getMessage());
-//
-    } finally {
+    }catch(IndexOutOfBoundsException err) {
+        System.out.println("존재하지 않는 스킬입니다. 기술 범위를 벗어남.");
+        System.out.println("예외 내용 " + err.getMessage());
+    }catch (Exception err){
+        System.out.println("예외가 발생했습니다.");
+        System.out.println("예외 내용 : " + err.getMessage());
+    }finally {
         System.out.println("프로그램 종료");
     }
 
