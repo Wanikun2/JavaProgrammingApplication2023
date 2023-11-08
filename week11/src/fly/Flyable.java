d28bda39428e6c9441972bef29933c2af67cffbd
Wanikun2 <aldalddl564@gmail.com>
2023-11-08 15:07:18 +0900

    week11 v49

---
+++ week11/src/fly/Flyable.java
@@ -0,0 +1,14 @@
+package fly;
+
+public interface Flyable {
+//    public fly.Flyable(){
+//
+//    }
+
+    //Pokemonster.Pokemon pokemon;
+
+   void fly(); //추상 메소드
+//    default void fly(){
+//        System.out.println("날아 갑니다!");
+//    }
+}
